ee36d141b4961f76bcc139610837d5c898140e56
Wiktor Lukasik <contact@wiktor.io>
2025-10-02 12:07:22 -0700

    test scripts to be removed later

---
+++ profiles/common/bin/test_existing.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "This is a file that already exists on the destination machine so you should NOT see this message!"
---
+++ profiles/common/bin/test_new.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "This is a new script and should be deployed to new machines!"
